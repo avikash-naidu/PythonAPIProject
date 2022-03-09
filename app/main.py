baf486e53642b62fa98b7567a813612418c67371
anai393 <anai393@aucklanduni.ac.nz>
2022-03-09 19:16:55 +1300

    updated requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from . import models
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
 from .routers import posts, users, auth, vote
 from .config import Settings
@@ -8,13 +8,27 @@ from app import database
 
 
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
+origins=[]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+
+@app.get('/')
+def root():
+    return {"msg": "hello world"}
